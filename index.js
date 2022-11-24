c177b911b972decf60ba20141e8ab7b873839be1
victor <victor.buthia@student.moringaschool.com>
2022-11-24 18:24:38 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(someWhere="roller-skate") {
+    return `This Saturday, I want to ${someWhere}!`
+    }
+    saturdayFun()
+
+ let mondayWork = function(willDo="go to the office"){
+     return `This Monday, I will ${willDo}.` 
+    }
+    mondayWork()
+
+    function wrapAdjective (style="*") {
+        return function(stringOne="special"){
+          return `You are ${style}${stringOne}${style}!`
+        }
+      }
